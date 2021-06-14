ad326ca5a73b384e6dbbff7eff68a6b11e0ebdee
udayuv <uday.lko67@gmail.com>
2021-06-14 11:10:35 +0530

    first commit

---
+++ src/main/java/com/uv/main/JenkinsExample.java
@@ -0,0 +1,17 @@
+package com.uv.main;
+
+import java.util.logging.Logger;
+
+
+public class JenkinsExample {
+
+	private static final Logger logger = Logger.getLogger(JenkinsExample.class.getName());
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	logger.info("APPLICATION EXECUTED....");	
+
+	}
+
+}
